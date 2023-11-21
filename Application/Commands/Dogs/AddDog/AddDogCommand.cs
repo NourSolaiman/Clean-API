8cad9a0abade2b48557379700b1d779de927e331
Nour Solaiman <solaiman.nour@gmail.com>
2023-11-21 19:51:21 +0100

    feature/dog-application-AddDog-command and handler

---
+++ Application/Commands/Dogs/AddDog/AddDogCommand.cs
@@ -0,0 +1,17 @@
+﻿using Application.Dtos;
+using Domain.Models;
+using MediatR;
+
+namespace Application.Commands.Dogs.AddDog
+{
+	public class AddDogCommand : IRequest<Dog>
+	{
+		public AddDogCommand(DogDto newDog)
+		{
+			NewDog = newDog;
+		}
+
+		public DogDto NewDog { get; }
+
+	}
+}
