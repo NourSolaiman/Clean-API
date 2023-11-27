76d9a86bfa9a0437d42c8109a333af1725606970
Nour Solaiman <solaiman.nour@gmail.com>
2023-11-27 15:52:35 +0100

    Adding Create a new bird/command and handler and in birds controller.

---
+++ Application/Commands/Birds/AddBirdCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Models;
+using Infrastructure.Database;
+using MediatR;
+
+namespace Application.Commands.Birds.AddBird
+{
+    public class AddBirdCommandHandler : IRequestHandler<AddBirdCommand, Bird>
+    {
+        private readonly MockDatabase _mockDatabase;
+
+        public AddBirdCommandHandler(MockDatabase mockDatabase)
+        {
+            _mockDatabase = mockDatabase;
+        }
+        public Task<Bird> Handle(AddBirdCommand request, CancellationToken cancellationToken)
+        {
+            Bird BirdToCreate = new()
+            {
+                animalId = Guid.NewGuid(),
+                Name = request.NewBird.Name
+            };
+
+            _mockDatabase.allBirds.Add(BirdToCreate);
+
+            return Task.FromResult(BirdToCreate);
+        }
+    }
+}
