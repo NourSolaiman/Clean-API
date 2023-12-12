19a5cbe3ee7a0217d7a4738766222195193b3ad7
Nour Solaiman <solaiman.nour@gmail.com>
2023-12-12 11:44:44 +0100

    Modifying AddBirdCommandHandler and it's tests

--- Application/Commands/Birds/AddBird/AddBirdCommandHandler.cs
+++ Application/Commands/Birds/AddBird/AddBirdCommandHandler.cs
@@ -1,28 +1,36 @@
 ﻿using Domain.Models;
-using Infrastructure.Database;
+using Infrastructure.Database.MySQLDatabase;
 using MediatR;
 
 namespace Application.Commands.Birds.AddBirds
 {
     public class AddBirdCommandHandler : IRequestHandler<AddBirdCommand, Bird>
     {
-        private readonly MockDatabase _mockDatabase;
+        private readonly caDBContext _caDBContext;
 
-        public AddBirdCommandHandler(MockDatabase mockDatabase)
+        public AddBirdCommandHandler(caDBContext caDBContext)
         {
-            _mockDatabase = mockDatabase;
+            _caDBContext = caDBContext;
         }
         public Task<Bird> Handle(AddBirdCommand request, CancellationToken cancellationToken)
         {
-            Bird BirdToCreate = new()
+            if (string.IsNullOrWhiteSpace(request.NewBird.Name) || request.NewBird.Name == "string")
             {
-                Id = Guid.NewGuid(),
-                Name = request.NewBird.Name
-            };
+                return Task.FromResult<Bird>(null!);
+            }
+            else
+            {
+                Bird BirdToCreate = new()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = request.NewBird.Name,
+                    CanFly = request.NewBird.CanFly,
+                };
+                _caDBContext.Birds.Add(BirdToCreate);
 
-            _mockDatabase.allBirds.Add(BirdToCreate);
+                return Task.FromResult(BirdToCreate);
 
-            return Task.FromResult(BirdToCreate);
+            }
         }
     }
 }
